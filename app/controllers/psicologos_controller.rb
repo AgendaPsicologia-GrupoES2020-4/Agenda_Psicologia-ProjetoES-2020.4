246b7c125330a5b655aea6cf5b739803694bf488
Gust4voSales <manoel0gustavo@gmail.com>
2021-02-02 13:08:51 -0300

    adicionar entidade de session para autenticacao da aplicacao

--- app/controllers/psicologos_controller.rb
+++ app/controllers/psicologos_controller.rb
@@ -1,8 +1,13 @@
 class PsicologosController < ApplicationController
+  skip_before_action :autorizado, only: [:new, :create]
+
   def new
     @psicologo = Psicologo.new
   end
 
   def create
+    @psicologo = Psicologo.create(params.require(:psicologo).permit(:crp, :nome, :password))
+    session[:user_id] = @psicologo.id
+    redirect_to '/principal'
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,32 @@
 class SessionsController < ApplicationController
+  skip_before_action :autorizado, only: [:new, :create, :landing]
+  
   def new
   end
 
   def create
+    @psicologo = Psicologo.find_by(crp: params[:crp])
+
+    if @psicologo && @psicologo.authenticate(params[:password])
+      session[:user_id] = @psicologo.id
+      redirect_to '/principal'
+    else
+      # flash.alert = "Usuário ou senha incorreto"
+      # redirect_to login_path, alert: "Usuário ou senha incorreto"
+      flash[:alert] = 'Usuário ou senha incorreto'
+      redirect_to login_path
+
+      # redirect_to login_path
+      # flash.now[:notice] = 'Successfully checked in'
+    end
   end
 
   def login
+  end
+
+  def destroy # logout
+    session[:user_id] = nil
+    redirect_to '/landing'
   end
   
   def landing
