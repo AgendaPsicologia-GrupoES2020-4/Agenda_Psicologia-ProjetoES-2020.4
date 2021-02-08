0583afecd65380a8f5e4ba1cebf04d6d46023ff7
Gust4voSales <manoel0gustavo@gmail.com>
2021-02-08 10:34:05 -0300

    adicionar teste de model p/ nao criar paciente com cpf em uso

--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -4,7 +4,7 @@ class PacienteTest < ActiveSupport::TestCase
   test "deve salvar paciente corretamente criado" do
     paciente = Paciente.new(
       "nome" => "Joao Pedro",
-      "cpf" => "702.345.344-43",
+      "cpf" => "254.674.365-43",
       "telefone" => "(81)99826454",
       "email" => 'joao@gmail.com',
       "endereco" => 'Av. Coronel Geminiano, 625',
@@ -19,4 +19,43 @@ class PacienteTest < ActiveSupport::TestCase
 
     assert paciente.save
   end
+
+  test "nao deve salvar paciente sem nome" do
+    paciente = Paciente.new(
+      "nome" => "",
+      "cpf" => "253.345.864-45",
+      "telefone" => "(81)99826454",
+      "email" => 'joao@gmail.com',
+      "endereco" => 'Av. Coronel Geminiano, 625',
+      "estado_civil" => 'Solteiro',
+      "quant_filhos" => 0,
+      "historico_doencas" => '',
+      "medicamentos" => 'Rivotril',
+      "eh_fumante" => 'Não',
+      "ingere_alcool" => 'Sim',
+      "psicologo_id" => psicologos(:psicologo_mock).id
+    )
+
+    assert_not paciente.save
+  end
+
+  test "nao deve salvar paciente com cpf em uso" do
+    paciente = Paciente.new(
+      "nome" => "Joao Pedro",
+      "cpf" => "702.345.344-45", #CPF já em uso, eu criei um fixture com um paciente com o mesmo cpf
+      "telefone" => "(81)99826454",
+      "email" => 'joao@gmail.com',
+      "endereco" => 'Av. Coronel Geminiano, 625',
+      "estado_civil" => 'Solteiro',
+      "quant_filhos" => 0,
+      "historico_doencas" => '',
+      "medicamentos" => 'Rivotril',
+      "eh_fumante" => 'Não',
+      "ingere_alcool" => 'Sim',
+      "psicologo_id" => psicologos(:psicologo_mock).id
+    )
+
+    assert_not paciente.save
+  end
+
 end
