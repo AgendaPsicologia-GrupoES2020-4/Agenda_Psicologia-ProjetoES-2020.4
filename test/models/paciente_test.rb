891c94dc76c8458f538bbe9fcc1e94c623059eb7
Gust4voSales <manoel0gustavo@gmail.com>
2021-02-08 10:21:07 -0300

    adicionar teste model p/ criar paciente corretamente

--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class PacienteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "deve salvar paciente corretamente criado" do
+    paciente = Paciente.new(
+      "nome" => "Joao Pedro",
+      "cpf" => "702.345.344-43",
+      "telefone" => "(81)99826454",
+      "email" => 'joao@gmail.com',
+      "endereco" => 'Av. Coronel Geminiano, 625',
+      "estado_civil" => 'Solteiro',
+      "quant_filhos" => 0,
+      "historico_doencas" => '',
+      "medicamentos" => 'Rivotril',
+      "eh_fumante" => 'Não',
+      "ingere_alcool" => 'Sim',
+      "psicologo_id" => psicologos(:psicologo_mock).id
+    )
+
+    assert paciente.save
+  end
 end
