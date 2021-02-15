14747c5fd0ec614197853ed8f8fdd5c2053b6109
Itamar Bernardo <itamarbernardo2013@gmail.com>
2021-02-15 20:55:45 -0300

    Adicionado validates para Sessao

--- app/models/sessao.rb
+++ app/models/sessao.rb
@@ -1,5 +1,7 @@
 class Sessao < ApplicationRecord
   belongs_to :paciente
   belongs_to :psicologo
-
+  validates :data, presence: true
+  validates :hora, presence: true
+  validates :paciente_id, presence: true
 end
